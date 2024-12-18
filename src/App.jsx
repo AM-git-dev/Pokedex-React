cbe0624e9fc74a2201980fdc223484008eec21fe
mix35 <pro.maximeandre@gmail.com>
2024-12-18 08:58:57 +0100

    save commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import { useState, useEffect }  from 'react'
 import './App.css'
+import axios from 'axios';
 
+function PokemonFetcher({ pokemonName }) {
+    const [pokemonData, setPokemonData] = useState(null);
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        if (!pokemonName) return;
+
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+            .then(response => {
+                setPokemonData(response.data);
+            })
+            .catch(err => {
+                setError(err);
+            });
+    }, [pokemonName]);
+
+    if (error) {
+        return <div>Erreur : {error.message}</div>;
+    }
+
+    if (!pokemonData) {
+        return <div>Chargement...</div>;
+    }
+
+    return (
+        <div>
+            <h1 style={{ textTransform: 'uppercase' }}>{pokemonData.name}</h1>
+            <img
+                src={pokemonData.sprites.front_default}
+                alt={pokemonData.name}
+            />
+            <p >Type principal : {pokemonData.types[0].type.name}</p>
+        </div>
+    );
+}
 function App() {
 
   return (
@@ -30,7 +65,13 @@ function App() {
                   <div className="bottomscreencontainer">
                       <div className="screenborder">
                           <div className="screen">
+                              <PokemonFetcher pokemonName="bulbasaur"></PokemonFetcher>
                           </div>
+                          <div className="buttonscontainer">
+                              <div className="start"></div>
+                              <div className="select"></div>
+                          </div>
+
                       </div>
                   </div>
                   <div className="rightborder">
